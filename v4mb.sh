069f6a55b8e34f9ffc457da5316bf4ddd9dc2869
xy364515640 <364515640@qq.com>
2021-05-09 20:12:38 +0800

    Update v4mb.sh

--- v4mb.sh
+++ v4mb.sh
@@ -1,4 +1,4 @@
-wget https://ghproxy.com/https://raw.githubusercontent.com/afwfv/dd/main/v4mb.tar.gz
+wget https://ghproxy.com/https://raw.githubusercontent.com/xy364515640/jd-new/main/v4mb.sh
 tar -zxvf v4mb.tar.gz
 rm -f v4mb.tar.gz
 mv v4mb/panel panel
@@ -8,4 +8,4 @@ rm -rf v4mb
 cd panel
 npm i
 pm2 start server.js
-echo -e "端口5678，默认用户名admin，密码adminadmin"
+echo -e "端口5800，默认用户名admin，密码adminadmin"
